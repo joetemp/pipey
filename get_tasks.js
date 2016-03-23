c00b9887fa32975883707ee1fd82d7854c954b7b
Joe Templeton <joe.k.templeton@gmail.com>
2016-03-23 14:48:08 -0600

    These programs now log all the application tasks. Now it needs to go through them and sort them.

--- get_tasks.js
+++ get_tasks.js
@@ -1,6 +1,25 @@
+const request = require('request');
 const getApps = require('./get_apps.js');
 
-getApps(function (id) {   
+getApps(function myFunctionName (id) {   
     // Do this stuff for each application.
-    console.log(id);
+    
+    var url = 'https://api.pipedrive.com/v1/activities?start=0&api_token=';
+    var apiToken = '07c87355d3f195c4b1bbfa6419456b620b575d05';
+
+    request(url + apiToken, function (error, response, body) {
+        if (error) {
+            return console.log(error);
+        }
+
+        var parse = JSON.parse(body);
+        var data = parse.data;
+        
+        for (var i in data) {
+            if (data[i].deal_id === id) {
+                console.log(data[i].subject);
+            } 
+        }
+    
+    })
 });
