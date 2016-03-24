086caad725137b56935475c5b12d03e134fa13e6
Joe Templeton <joe.k.templeton@gmail.com>
2016-03-24 17:37:28 -0600

    Newest.js kind of works. At least it logs the whole stageDeal array. Now I just need to get rid of 21 from that array, since it already has a 4506-T task.

---
+++ newest.js
@@ -0,0 +1,44 @@
+const request = require('request');
+
+var url = {deals: 'https://api.pipedrive.com/v1/deals?start=0&api_token=07c87355d3f195c4b1bbfa6419456b620b575d05', 
+           activities: 'https://api.pipedrive.com/v1/activities?start=0&api_token=07c87355d3f195c4b1bbfa6419456b620b575d05'};
+
+function getDeals (url, key, value, callback) {
+    
+    request(url, function (error, response, body) {
+        if (error) {
+            return console.log(error);
+        }  
+
+        var parse = JSON.parse(body).data;
+        
+        callback(parse);
+    });
+}
+
+getDeals (url.deals, 'stage_id', 2, function (deals) {
+
+    getDeals (url.activities, 'subject', '4506-T', function (activities) {
+
+        var stageDeals = [];
+
+        for (var i in deals) {
+            if (deals[i].stage_id === 2) {
+                stageDeals.push(deals[i].id);
+                 console.log(stageDeals);
+            }
+        }
+
+        for (var j in stageDeals) {
+            
+            for (var i in activities) {
+            
+                if (activities[i].subject === '4506-T' && stageDeals[j].id === activities[i].deal_id) {
+                    // Delete the deal from the array.
+                }
+            }
+        }
+    
+    });
+
+}); 
