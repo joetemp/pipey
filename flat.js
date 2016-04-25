0909a2ce6015563740852032b4ac3cdb6e8d0b54
Joe Templeton <joe.k.templeton@gmail.com>
2016-04-25 16:23:21 -0600

    Committing to show shizy the api key that is weird.

---
+++ flat.js
@@ -0,0 +1,71 @@
+const request = require('request-promise');
+const moment = require('moment');
+moment().format();
+
+var API_KEY = process.env.API_KEY;
+
+var urls = { deals: 'https://api.pipedrive.com/v1/deals?start=0&api_token=' + API_KEY,
+             activities: 'https://api.pipedrive.com/v1/activities?start=0&api_token=' + API_KEY };
+
+function getIt (url) {
+    return request(url).then(function (body) {
+        return JSON.parse(body).data;
+    });
+} 
+
+Promise.all([getIt(urls.deals), getIt(urls.activities)]).then(function(results) {
+
+    var deals = results[0] || [];
+    var activities = results[1] || [];
+
+    var apps = {};
+    var haves = {};
+    var test = {};
+
+    deals.forEach(function(deal){
+	if (deal.stage_id === 2) {
+	    apps[deal.id] = deal;
+	} 
+    });
+
+    deals.forEach(function(deal){
+        if (deal[33eb86af817c62123047fc43d6afe908adbd203d]  === 2) {
+            test[deal.id] = deal; 
+        }       
+    });
+
+
+    activities.forEach(function(activity){
+	if (activity.subject === '4506-T') {
+	    haves[activity.deal_id] = activity; 
+	}
+    });
+
+    console.log(Object.keys(test));
+    
+    //console.log(Object.keys(apps));
+    //console.log(Object.keys(haves));
+
+    var diffs = Object.keys(apps).filter(function(app) {
+	return (Object.keys(haves).indexOf(app) === -1); 
+    });
+
+    var haveNots = diffs.map(function(item){
+	return Number(item); 
+    });
+
+    //console.log(haveNots);
+
+    haveNots.forEach(function(deal){
+	request.post('https://api.pipedrive.com/v1/activities?api_token=' + API_KEY, {
+	    form: {'subject': '4506-T',
+		   'deal_id': deal,
+		     'note' : apps[deal].person_id.name + ' is a pretty cool dude.',
+		 'due_date' : moment(apps[deal].add_time).add(3, 'days').format('YYYY-MM-DD')}});
+
+	console.log(apps[deal].person_id.name + ' was created on ' + moment(apps[deal].add_time).format('YYYY-MM-DD') + '. So, this task is due on ' + moment(apps[deal].add_time).add(3, 'days').format('YYYY-MM-DD') + '.');
+    });
+
+}).then(function(){
+// do more stuff here.
+});
