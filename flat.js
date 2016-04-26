efaaf7bf7657b52f5dcd1b784195f4f131d52bd4
Joe Templeton <joe.k.templeton@gmail.com>
2016-04-25 22:30:03 -0600

    flat.js has been cleaned up.

--- flat.js
+++ flat.js
@@ -18,17 +18,11 @@ Promise.all([getIt(urls.deals), getIt(urls.activities)]).then(function(results) 
     var deals = results[0] || [];
     var activities = results[1] || [];
 
+    // Custom Fields
     var type = '33eb86af817c62123047fc43d6afe908adbd203d';
 
-    var apps = {};
-    var haves = {};
+    var has4506T = {};
     var realDeals = {};
-
-    deals.forEach(function(deal){
-	if (deal.stage_id === 2) {
-	    apps[deal.id] = deal;
-	} 
-    });
 
     deals.forEach(function(deal){
         if (deal[type] === '1' && deal.stage_id === 2) {
@@ -41,42 +35,29 @@ Promise.all([getIt(urls.deals), getIt(urls.activities)]).then(function(results) 
 
     activities.forEach(function(activity){
 	if (activity.subject === '4506-T') {
-	    haves[activity.deal_id] = activity; 
+	    has4506T[activity.deal_id] = activity; 
 	}
     });
-    
-    // console.log(Object.keys(apps));
-    // console.log(Object.keys(haves));
 
-    var diffs = Object.keys(apps).filter(function(app) {
-	return (Object.keys(haves).indexOf(app) === -1); 
+    var diff4506T = Object.keys(realDeals).filter(function(realDeal) {
+        return (Object.keys(has4506T).indexOf(realDeal) === -1); 
     });
 
-    var diffy = Object.keys(realDeals).filter(function(realDeal) {
-        return (Object.keys(haves).indexOf(realDeal) === -1); 
-    });
-
-    var realDealHaveNots = diffy.map(function(item){
+    var realDealNeeds4506T = diff4506T.map(function(item){
         return Number(item); 
     });
 
     console.log('Real deals that need activities: ');
-    console.log(realDealHaveNots);
+    console.log(realDealNeeds4506T);
 
-    var haveNots = diffs.map(function(item){
-	return Number(item); 
-    });
-
-    // console.log(haveNots);
-
-    realDealHaveNots.forEach(function(deal){
+    realDealNeeds4506T.forEach(function(deal){
 	request.post('https://api.pipedrive.com/v1/activities?api_token=' + API_KEY, {
 	    form: {'subject': '4506-T',
 		   'deal_id': deal,
-		     'note' : apps[deal].person_id.name + ' is a pretty cool dude.',
-		 'due_date' : moment(apps[deal].add_time).add(3, 'days').format('YYYY-MM-DD')}});
+		     'note' : realDeals[deal].person_id.name + ' is a pretty cool dude.',
+		 'due_date' : moment(realDeals[deal].add_time).add(3, 'days').format('YYYY-MM-DD')}});
 
-	console.log(apps[deal].person_id.name + ' was created on ' + moment(apps[deal].add_time).format('YYYY-MM-DD') + '. So, this task is due on ' + moment(apps[deal].add_time).add(3, 'days').format('YYYY-MM-DD') + '.');
+	console.log(realDeals[deal].person_id.name + ' was created on ' + moment(realDeals[deal].add_time).format('YYYY-MM-DD') + '. So, this task is due on ' + moment(realDeals[deal].add_time).add(3, 'days').format('YYYY-MM-DD') + '.');
     });
 
 }).then(function(){
