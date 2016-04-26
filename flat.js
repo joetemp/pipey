2ced413a4a06560ce5eefe3de3059e1ca0589842
Joe Templeton <joe.k.templeton@gmail.com>
2016-04-25 22:12:35 -0600

    Program is ugly as shit... but it only adds 4506-T activities to real deals now.

--- flat.js
+++ flat.js
@@ -18,8 +18,11 @@ Promise.all([getIt(urls.deals), getIt(urls.activities)]).then(function(results) 
     var deals = results[0] || [];
     var activities = results[1] || [];
 
+    var type = '33eb86af817c62123047fc43d6afe908adbd203d';
+
     var apps = {};
     var haves = {};
+    var realDeals = {};
 
     deals.forEach(function(deal){
 	if (deal.stage_id === 2) {
@@ -27,26 +30,46 @@ Promise.all([getIt(urls.deals), getIt(urls.activities)]).then(function(results) 
 	} 
     });
 
+    deals.forEach(function(deal){
+        if (deal[type] === '1' && deal.stage_id === 2) {
+            realDeals[deal.id] = deal; 
+        } 
+    });
+
+    console.log('Real deals: ');
+    console.log(Object.keys(realDeals));
+
     activities.forEach(function(activity){
 	if (activity.subject === '4506-T') {
 	    haves[activity.deal_id] = activity; 
 	}
     });
     
-    console.log(Object.keys(apps));
-    console.log(Object.keys(haves));
+    // console.log(Object.keys(apps));
+    // console.log(Object.keys(haves));
 
     var diffs = Object.keys(apps).filter(function(app) {
 	return (Object.keys(haves).indexOf(app) === -1); 
     });
 
+    var diffy = Object.keys(realDeals).filter(function(realDeal) {
+        return (Object.keys(haves).indexOf(realDeal) === -1); 
+    });
+
+    var realDealHaveNots = diffy.map(function(item){
+        return Number(item); 
+    });
+
+    console.log('Real deals that need activities: ');
+    console.log(realDealHaveNots);
+
     var haveNots = diffs.map(function(item){
 	return Number(item); 
     });
 
-    console.log(haveNots);
+    // console.log(haveNots);
 
-    haveNots.forEach(function(deal){
+    realDealHaveNots.forEach(function(deal){
 	request.post('https://api.pipedrive.com/v1/activities?api_token=' + API_KEY, {
 	    form: {'subject': '4506-T',
 		   'deal_id': deal,
